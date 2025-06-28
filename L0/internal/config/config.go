35290e07102068decc05bdd6b376f09635014bbb
Ilya Gellert <gellertilya@gmail.com>
2025-06-28 18:57:52 +0300

    Create config structers

--- L0/internal/config/config.go
+++ L0/internal/config/config.go
@@ -1,1 +1,42 @@
 package config
+
+import (
+	"database/sql"
+	"github.com/spf13/viper"
+)
+
+type DSN string
+
+type PostgresShardConfig struct {
+	Primary  DSN   `yaml:"primary"`
+	Replicas []DSN `yaml:"replicas"`
+}
+
+type PostgresConfig struct {
+	LookupDbDsn DSN                            `yaml:"lookup_db_dsn"`
+	Shards      map[string]PostgresShardConfig `yaml:"shards"`
+}
+
+type RedisConfig struct {
+	Addr string `yaml:"addr"`
+}
+
+type KafkaConfig struct {
+	Brokers []string `yaml:"brokers"`
+	Topic   string   `yaml:"topic"`
+}
+
+type ServerConfig struct {
+	Port string `yaml:"port"`
+}
+
+type Config struct {
+	Postgres PostgresConfig `yaml:"postgres"`
+	Redis    RedisConfig    `yaml:"redis"`
+	Kafka    KafkaConfig    `yaml:"kafka"`
+	Server   ServerConfig   `yaml:"server"`
+}
+
+func NewConfig() *Config {
+
+}
--- L0/internal/domain/order_repository.go
+++ L0/internal/domain/order_repository.go
@@ -7,6 +7,8 @@ import (
 )
 
 type OrderRepository interface {
-	GetByUID(ctx context.Context, uid uuid.UUID) (*domain.Order, error)
 	Save(ctx context.Context, order *domain.Order) error
+
+	GetByUID(ctx context.Context, uid uuid.UUID) (*domain.Order, error)
+	Get
 }
