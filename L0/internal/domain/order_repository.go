eb2abfe25dae9f542dc3d314efa872d2635df493
Ilya Gellert <gellertilya@gmail.com>
2025-06-30 16:56:10 +0300

    Update OrderRepository interface, add GetLatest

--- L0/internal/domain/order_repository.go
+++ L0/internal/domain/order_repository.go
@@ -1,14 +1,12 @@
-package repository
+package domain
 
 import (
-	"L0/internal/domain"
 	"context"
 	"github.com/google/uuid"
 )
 
 type OrderRepository interface {
-	Save(ctx context.Context, order *domain.Order) error
-
-	GetByUID(ctx context.Context, uid uuid.UUID) (*domain.Order, error)
-	Get
+	Save(ctx context.Context, order *Order) error
+	GetByUID(ctx context.Context, uid uuid.UUID) (*Order, error)
+	GetLatest(ctx context.Context, limit uint) ([]*Order, error)
 }
