7ba4c2c98340d3f78c1a54ade69d472258780d4d
Ilya Gellert <gellertilya@gmail.com>
2025-07-07 19:10:34 +0300

    Create consumer

--- L0/internal/transport/tkafka/consumer.go
+++ L0/internal/transport/tkafka/consumer.go
@@ -6,10 +6,14 @@ import (
 	"L0/internal/metrics"
 	"context"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 	"github.com/sirupsen/logrus"
 	"time"
 )
+
+var ErrInvalidMessageData = errors.New("invalid message data")
 
 type Topics []string
 
@@ -33,6 +37,23 @@ func NewConsumer(cfg *kafka.ConfigMap, service app.OrderService, logger *logrus.
 	}, nil
 }
 
+func (c *Consumer) handleSingleMessage(ctx context.Context, msg *kafka.Message) error {
+	order := domain.Order{}
+	if err := json.Unmarshal(msg.Value, &order); err != nil {
+		metrics.KafkaMessagesInvalidTotal.Inc()
+		return fmt.Errorf("unmarshal error (bad message): %w", err)
+	}
+	if order.Delivery == nil || order.Payment == nil {
+		metrics.KafkaMessagesInvalidTotal.Inc()
+		c.logger.Errorf("invalid order data: delivery or payment is null for order %s", order.OrderUID)
+		return fmt.Errorf("%w: delivery or payment is null for order %s", ErrInvalidMessageData, order.OrderUID)
+	}
+	if err := c.orderService.ProcessNewOrder(ctx, &order); err != nil {
+		return fmt.Errorf("service processing error: %w", err)
+	}
+	return nil
+}
+
 func (c *Consumer) StartConsuming(ctx context.Context, subscribingTopic Topics) {
 	err := c.kafkaConsumer.SubscribeTopics(subscribingTopic, nil)
 	if err != nil {
@@ -46,31 +67,20 @@ func (c *Consumer) StartConsuming(ctx context.Context, subscribingTopic Topics) 
 			msg, err := c.kafkaConsumer.ReadMessage(1 * time.Second)
 			if err == nil {
 				metrics.KafkaMessagesReceivedTotal.Inc()
-
-				orderBytes := msg.Value
-				order := domain.Order{}
-				if err = json.Unmarshal(orderBytes, &order); err != nil {
-					c.logger.Errorf("failed to unmarshal order: %v", err)
-					metrics.KafkaMessagesInvalidTotal.Inc()
-					continue
-				}
-				if order.Delivery == nil || order.Payment == nil {
-					c.logger.Errorf("invalid order data: delivery or payment is null for order %s", order.OrderUID)
-					if _, err = c.kafkaConsumer.CommitMessage(msg); err != nil {
-						c.logger.Errorf("failed to commit bad message: %v", err)
+				if err = c.handleSingleMessage(ctx, msg); err != nil {
+					if errors.Is(err, ErrInvalidMessageData) {
+						c.logger.Warn("Skipping and committing bad message...")
+						_, _ = c.kafkaConsumer.CommitMessage(msg)
+						continue
 					}
+					c.logger.WithField("order_uid", msg.Key).Errorf("Failed to handle message: %v", err)
 					continue
+				} else {
+					_, err = c.kafkaConsumer.CommitMessage(msg)
+					if err != nil {
+						c.logger.Errorf("Failed to commit message: %v", err)
+					}
 				}
-				if err = c.orderService.ProcessNewOrder(ctx, &order); err != nil {
-					c.logger.Errorf("failed to save order: %v", err)
-					continue
-				}
-				if _, err = c.kafkaConsumer.CommitMessage(msg); err != nil {
-					c.logger.Errorf("failed to commit message: %v", err)
-					metrics.KafkaMessagesInvalidTotal.Inc()
-					continue
-				}
-				c.logger.Infof("consumed order: %v", order)
 
 			} else if kafkaErr, ok := err.(kafka.Error); ok && kafkaErr.Code() == kafka.ErrTimedOut {
 				continue
